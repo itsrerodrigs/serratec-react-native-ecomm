6f715d76de518a77807e12a4f7ce8180b814bbab
LucasBerlim <berlimtere@gmail.com>
2024-11-20 02:22:13 -0300

    Add screen Detalhes and Setup GET, DELETE products

--- src/components/CardProduto/CardProduto.tsx
+++ src/components/CardProduto/CardProduto.tsx
@@ -1,18 +1,22 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { HomeScreenProps } from "../../screens/home";
 
-type Cardproduto = {
+type CardProdutoProps = {
+    id: string,
     imagem: string,
     nome: string,
     quantidade: number,
     valor: number,
-}
+    navigation: HomeScreenProps['navigation'],
+    onPress: () => void,
+};
 
-export const CardProduto = ({ imagem, nome, quantidade, valor}: Cardproduto) => {
+export const CardProduto = ({ id, imagem, nome, quantidade, valor, navigation, onPress }: CardProdutoProps) => {
     return (
         <View style={styles.container}>
             <View style={styles.containerItem}>
-                <Image source={{uri: imagem}} style={styles.imagem} />
+                <Image source={{ uri: imagem }} style={styles.imagem} />
                 <View style={styles.details}>
                     <Text style={styles.text}>{nome}</Text>
                     <Text style={styles.text}>Quantidade: {quantidade}</Text>
@@ -21,11 +25,11 @@ export const CardProduto = ({ imagem, nome, quantidade, valor}: Cardproduto) => 
             </View>
 
             <View style={styles.containerIcons}>
-                <TouchableOpacity style={styles.ButtonIcon}>
-                    <FontAwesome6 name="edit" size={40} color="black" />
+                <TouchableOpacity style={styles.ButtonIcon} onPress={() => navigation.navigate("Detalhes", { id: id, nome: nome })}>
+                    <FontAwesome6 name="edit" size={30} color="white" />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.ButtonIcon}>
-                    <FontAwesome6 name="trash-can" size={40} color="black" />
+                <TouchableOpacity style={styles.ButtonIcon} onPress={onPress}>
+                    <FontAwesome6 name="trash-can" size={30} color="white" />
                 </TouchableOpacity>
             </View>
         </View>
@@ -39,7 +43,6 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
         padding: 10,
-        backgroundColor: "lightgray",
     },
     containerItem: {
         flexDirection: "row",
@@ -48,15 +51,15 @@ const styles = StyleSheet.create({
         width: "70%",
     },
     imagem: {
-        width: 100,
-        height: 100,
+        width: 120,
+        height: 120,
         borderRadius: 12,
     },
     details: {
         flexDirection: "column",
         justifyContent: "flex-start",
         alignContent: "flex-start",
-        marginLeft: 10,
+        marginLeft: 16,
     },
     containerIcons: {
         flexDirection: "row",
@@ -66,6 +69,8 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 16,
+        fontWeight: "500",
+        color: "white",
     },
     ButtonIcon: {
         marginHorizontal: 5,
--- src/components/FlatListProdutos/FlatListProdutos.tsx
+++ src/components/FlatListProdutos/FlatListProdutos.tsx
@@ -1,60 +1,126 @@
-import { FlatList, View, StyleSheet } from "react-native"
-import { CardProduto } from "../CardProduto/CardProduto"
-import { useState } from "react";
+import { FlatList, View, StyleSheet, Text, ActivityIndicator, Alert } from "react-native";
+import { CardProduto } from "../CardProduto/CardProduto";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { HomeScreenProps } from "../../screens/home";
 
-const data = [
-    {
-        imagem: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQjUn8ZlKl__snd2CUhBrsRuQlKiQVJs_EMRHuCvMn18bRPMQ7iSl8ii295rmKn5XslJvBhxecxmj0vYuS1mx0XOClEaV6v2g&usqp=CAE",
-        nome: "Funko 1607",
-        quantidade: 18,
-        valor: 110.90
-    },
-    {
-        imagem: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSE_N5Oj8d81ubvJAUFLIU5lbYqYgP0wlvvDa0p5xmzPfMEG1D7P5pBL0I8SC7v4FpZQmAr1TbX5TNMi8Q4xgqC9atiHu1rwCI73vvJpBQOOHFLdFrK3hAO&usqp=CAE",
-        nome: "Funko 1341",
-        quantidade: 18,
-        valor: 110.90
-    },
-]
+const URL = "https://673bbe7b96b8dcd5f3f74e80.mockapi.io/api";
 
-/*const [loading, setloading] =  useState(true);
+type Produto = {
+    id: string,
+    imagem: string,
+    nome: string,
+    quantidade: number,
+    valor: number,
+};
 
-const obterDados = async () => {
-    setloading(true);
-    try {
-        //const { data } = await axios
-    } catch (err) {
-        console.log("Erro ao carregar Tarefas. ", err)
-    }
-}*/
+type FlatListProdutosProps = {
+    navigation: HomeScreenProps['navigation'];
+};
 
 const ItemSeparator = () => { return <View style={styles.separator} />; };
 
-export const FlatListProdutos = () => {
+export const FlatListProdutos = ({ navigation }: FlatListProdutosProps) => {
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const obterDados = async () => {
+        setLoading(true);
+        try {
+            const { data } = await axios.get(`${URL}/produtos`);
+            setProdutos(data);
+        } catch (err) {
+            console.log("Erro ao carregar produtos. ", err);
+        }
+        setLoading(false);
+    };
+
+    const deleteProduto = async (id: string) => {
+        const confirmed = await showConfirmDialog(); 
+        if (!confirmed) { 
+            return;
+        }
+        try {
+            const { data } = await axios.delete(`${URL}/produtos/${id}`);
+            const newList = produtos.filter((item) => item.id !== data.id);
+            setProdutos(newList);
+        } catch (err) {
+            console.log("Erro ao tentar excluir o Produto. ", err);
+        }
+    }
+
+    const showConfirmDialog = () => {
+        return new Promise<boolean>((bool) => {
+            Alert.alert(
+                "Confirmar",
+                "Deseja mesmo deletar este item?",
+                [
+                    {
+                        text: "Cancelar",
+                        onPress: () => bool(false),
+                    },
+                    {
+                        text: "OK",
+                        onPress: () => bool(true),
+                    }
+                ]
+            );
+        });
+    };    
+
+    useEffect(() => {
+        obterDados();
+    }, []);
+
     return (
-        <View style={styles.container}>
-             
-            <FlatList
-                data={data}
-                renderItem={({ item, index}) => (
-                    <>
-                        <CardProduto imagem={item.imagem} nome={item.nome} quantidade={item.quantidade} valor={item.valor}/>
-                    </>
+        <>
+            <Text style={styles.titulo}>Produtos</Text>
+            <View style={styles.container}>
+                {loading ? (
+                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                        <ActivityIndicator size="large" />
+                    </View>
+                ) : (
+                    <FlatList
+                        data={produtos}
+                        renderItem={({ item }) => (
+                            <CardProduto
+                                id={item.id}
+                                imagem={item.imagem}
+                                nome={item.nome}
+                                quantidade={item.quantidade}
+                                valor={item.valor}
+                                navigation={navigation}
+                                onPress={() => deleteProduto(item.id)}
+                            />
+                        )}
+                        ItemSeparatorComponent={ItemSeparator}
+                        keyExtractor={(item) => item.id}
+                    />
                 )}
-                ItemSeparatorComponent={ItemSeparator}
-            />
-        </View>
-    )
-}
+            </View>
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
+    titulo: {
+        marginVertical: 20,
+        marginHorizontal: 10,
+        fontSize: 18,
+        textAlign: "center",
+        fontWeight: "500",
+    },
     container: {
-        backgroundColor: "lightgray"
+        backgroundColor: "#355C7D",
     },
     separator: {
-        height: 2, 
-        backgroundColor: 'gray', 
+        height: 2,
+        backgroundColor: 'gray',
         marginHorizontal: 10,
     },
+});
+
+
+
     
-})
