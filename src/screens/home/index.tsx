f8073244ee518dadcc4035cd5108b44857901a01
romulo.louremco <romulo.lourenco1@gmail.com>
2024-11-21 14:51:33 -0300

    add botão cadastro

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { useAuth } from "../../context/auth";
 import { CardProduto } from "../../components/CardProduto/CardProduto";
 import { FlatListProdutos } from "../../components/FlatListProdutos/FlatListProdutos";
@@ -17,8 +17,22 @@ export const Home = ({ navigation }: HomeScreenProps) => {
     return (
         <>
             <Button title="Logout" onPress={signOut} />
-            <Button title="Cadastrar produto" onPress={() => navigation.navigate("CadastroProduto")} />
+            <TouchableOpacity style={styles.botaoCadastrar} onPress={ () => navigation.navigate("CadastroProduto")}>  
+                <Text>Pressione aqui</Text>
+            </TouchableOpacity> 
             <FlatListProdutos navigation={navigation} />
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    botaoCadastrar: {
+        padding: 20,
+        width: "100%",
+        backgroundColor: "#bdd3ce",
+        borderColor: "#43d3aa",
+        borderWidth: 1,
+        alignItems: "center",
+        borderRadius: 5,
+    }
+})
