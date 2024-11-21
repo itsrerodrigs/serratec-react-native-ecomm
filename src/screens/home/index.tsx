82af09fe12296a7868a0c8de3c16fecf50a96426
LucasBerlim <berlimtere@gmail.com>
2024-11-21 15:04:53 -0300

    Update home page

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useAuth } from "../../context/auth";
 import { CardProduto } from "../../components/CardProduto/CardProduto";
 import { FlatListProdutos } from "../../components/FlatListProdutos/FlatListProdutos";
@@ -15,17 +15,30 @@ export const Home = ({ navigation }: HomeScreenProps) => {
     const { signOut } = useAuth();
 
     return (
-        <>
+        <View style={styles.container}>
             <Button title="Logout" onPress={signOut} />
-            <TouchableOpacity style={styles.botaoCadastrar} onPress={ () => navigation.navigate("CadastroProduto")}>  
-                <Text>Pressione aqui</Text>
-            </TouchableOpacity> 
+            <View style={styles.containerMenu}>
+                <TouchableOpacity style={styles.botaoCadastrar} onPress={() => navigation.navigate("CadastroProduto")}>  
+                    <Text style={styles.textBotaoCadastrar}>Cadastrar novo produto</Text>
+                </TouchableOpacity> 
+            </View>
+            
             <FlatListProdutos navigation={navigation} />
-        </>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        gap: 15,
+    },
+    containerMenu: {
+        width: "40%",
+        justifyContent: "center",
+        alignItems: "center",
+       
+    },
     botaoCadastrar: {
         padding: 20,
         width: "100%",
@@ -34,5 +47,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         alignItems: "center",
         borderRadius: 5,
+    }, 
+    textBotaoCadastrar: {
+        textAlign: "center",
     }
 })
