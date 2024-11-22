3ed3f19383c4b612ca66c20b2432a01d9db18f74
eumichelems <michelemsilva28@gmail.com>
2024-11-22 16:29:15 -0300

    Criacao do SplashScreen e alteracao no app.json

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/context/auth';
-import { Routes } from './src/routes';
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/context/auth";
+import { Routes } from "./src/routes";
+import { SplashScreen } from "./src/screens/SplashScreen/SplashScreen";
+import { useState } from "react";
 
 export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashFinish = () => {
+    setShowSplash(false);
+  };
+
+  if (showSplash) {
+    return <SplashScreen onFinish={handleSplashFinish} />;
+  }
+
   return (
     <NavigationContainer>
-        <AuthProvider>
-          <Routes/>
-        </AuthProvider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
